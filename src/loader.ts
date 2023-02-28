4320fb6b0b49d140c45c4be9e5be23509aa5e5a2
Albert Ruiz <ru.albert@gmail.com>
2023-02-28 20:57:15 +0100

    update

--- src/loader.ts
+++ src/loader.ts
@@ -4,8 +4,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 type LoadedModelRecord = {
   url: string;
   model?: Group;
-  startTime: number;
-  endTime?: number;
+  startTime: Date;
+  endTime?: Date;
 };
 
 export class LoadedModelRegister {
@@ -27,7 +27,7 @@ export class LoadedModelRegister {
       throw `${key} is already loading`;
     }
 
-    this.records[key] = { url: url, startTime: Date.now() };
+    this.records[key] = { url: url, startTime: new Date(Date.now()) };
   }
 
   loaded(key: string, model: Group) {
@@ -36,7 +36,7 @@ export class LoadedModelRegister {
     }
 
     this.records[key].model = model;
-    this.records[key].endTime = Date.now();
+    this.records[key].endTime = new Date(Date.now());
   }
 
   getModel(key: string) {
@@ -44,7 +44,7 @@ export class LoadedModelRegister {
       return this.records[key].model;
     }
 
-    throw `${key} is unknown`;
+    throw `${key} is unknown or not loaded`;
   }
 
   getUrl(key: string) {
@@ -52,7 +52,15 @@ export class LoadedModelRegister {
       return this.records[key].url;
     }
 
-    throw `${key} is unknown`;
+    throw `${key} is unknown or not loaded`;
+  }
+
+  getLoadTime(key: string) {
+    if (this.has(key) && this.isLoaded(key)) {
+      return this.records[key].startTime
+    }
+
+    throw `${key} is unknown or not loaded`;
   }
 
   isEmpty() {
@@ -134,33 +142,37 @@ export class ModelLoader {
     return this.register.getUrl(key);
   }
 
+  getLoadTime(key: string) {
+    return this.register.getLoadTime(key)
+  }
+
   allLoaded() {
     return this.register.allLoaded();
   }
 
-  // async waitFor(key: string, delay: number = 100) {
+  async waitFor(key: string, delay: number = 10000000) {
 
-  //   for (let i = 0 ; i < 100 ; i++ ){
-  //     if (this.register.isLoaded(key)) {
-  //       return;
-  //     }
+    for (let i = 0 ; i < 100 ; i++ ){
+      if (this.register.isLoaded(key)) {
+        return;
+      }
 
-  //     console.log(`Waiting for ${key}`);
-  //     await this.sleep(delay);  
-  //   }
-    
-  //   throw 'foo'
-  // }
-
-  // private sleep(ms: number) {
-  //   return new Promise((resolve) => setTimeout(resolve, ms));
-  // }
-
-  waitFor(key: string, delay: number = 100) {
-    if (!this.register.isLoaded(key)) {
-      setTimeout((x) => {this.waitFor(x)}, delay, key)
+      console.log(`Waiting for ${key}`);
+      await this.sleep(delay);  
     }
+    
+    throw 'foo'
   }
+
+  private sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  // waitFor(key: string, delay: number = 100) {
+  //   if (!this.register.isLoaded(key)) {
+  //     setTimeout((x) => {this.waitFor(x)}, delay, key)
+  //   }
+  // }
 
 
   speak() {
