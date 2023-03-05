e2fed9428009cbf92578383249ece4a18216c62d
Albert Ruiz <ru.albert@gmail.com>
2023-03-05 09:19:53 +0100

    update

--- src/car.ts
+++ src/car.ts
@@ -4,32 +4,52 @@ export class Car {
   private _model: Group;
   private _speed: number;
   private _acceleration: number;
+  private _maxSpeed: number
+  private _maxAcceleration: number
 
   constructor(model: Group) {
     this._model = model;
     this._speed = 0.0;
     this._acceleration = 0.0;
+
+    this._maxSpeed = 10;
+    this._maxAcceleration = 3;
   }
 
   get model() {
     return this._model;
   }
 
-  update(up: boolean, down: boolean, right: boolean, left: boolean) {
-    if (up && !down) {
-      console.log('accelerate');
-    } else if (down && !up) {
-      console.log('break`');
-    } else if (up && down) {
-      console.log('no effect accelerate/break');
-    }
-
-    if (right && !left) {
-      console.log('turn right');
-    } else if (left && !right) {
-      console.log('turn left');
-    } else if (right && left) {
-      console.log('no effect right/left');
+  update(keyCode: string = '') {
+    switch (keyCode) {
+      case 'ArrowUp':
+        this.accelerate()
+        break;
+      case 'ArrowDown':
+        this.accelerate(-0.5)
+        break;
+      case 'ArrowRight':
+        console.log('Turn right')
+        break
+      default:
+        this.accelerate(-0.01)
+        break;
     }
   }
+
+  accelerate(delta: number = 0.1) {
+    console.log(`accelerate by ${delta}`);
+    if (delta > 0) {
+      this._acceleration = Math.min(this._acceleration + delta, this._maxAcceleration)
+    }
+
+    if (delta < 0) {
+      this._acceleration = Math.max(this._acceleration + delta, 0)
+    }
+    console.log(`acceleration is ${this._acceleration}`)
+  }
 }
+
+// class KeyboardInput {
+//   constructor
+// }
